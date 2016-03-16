e405942fc893bd37b32effba0b49a8332039fe7a
Justin Brubaker <justbru00@gmail.com>
2016-03-15 21:24:21 -0400

    Added basic plugin files. Workspace ready for programming.

---
+++ src/com/gmail/justbru00/epic/enchants/main/Main.java
@@ -0,0 +1,25 @@
+package com.gmail.justbru00.epic.enchants.main;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		return false;
+	}
+
+	@Override
+	public void onDisable() {
+	
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+}
