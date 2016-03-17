6f673cb9581765a83717b3a10a91aeb002b42769
Justin Brubaker <justbru00@gmail.com>
2016-03-17 07:38:58 -0400

    Added some stuff :smile:

--- src/com/gmail/justbru00/epic/enchants/main/Main.java
+++ src/com/gmail/justbru00/epic/enchants/main/Main.java
@@ -1,10 +1,17 @@
 package com.gmail.justbru00.epic.enchants.main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.gmail.justbru00.epic.enchants.utils.Messager;
+
 public class Main extends JavaPlugin {
+	
+	public static String PREFIX = Messager.color("&8[&bEpic&fEnchants&8] &f");
+	public static ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
